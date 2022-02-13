b50819c2c05175d8c3ca5cfe46d7063feda84b05
johanMuneton <johanmudea@gmail.com>
2022-02-13 16:53:31 -0500

    punto 17 completo.

---
+++ Electrodomesticos/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+S
+#src/co/com/jm/Electrodomestico.java,c/3/c3fd6da0fb95675065d073b386d47106d681ecd8
+M
+src/co/com/jm/Television.java,5/a/5a450a09139821fd778b4ee7b4d0cead9a838180
+K
+src/co/com/jm/Lavadora.java,3/b/3bfd3680bd332edfe987eb066139c79adcacfd4a
+G
+src/co/com/jm/Main.java,6/0/60a262e180660d0c4e120865e7e40d474ee313db
--- Electrodomesticos/src/co/com/jm/Main.java
+++ Electrodomesticos/src/co/com/jm/Main.java
@@ -7,32 +7,34 @@ public class Main {
 
 
         Electrodomestico arrayObjetos[] = new Electrodomestico[10];
-
         arrayObjetos[0] = new Television(150, 50);
-
         arrayObjetos[1] = new Lavadora(222, 99);
         arrayObjetos[2] = new Television(120, 40);
-
         arrayObjetos[3] = new Lavadora(200,100);
-
         arrayObjetos[4] = new Television(800, 20);
-
         arrayObjetos[5] = new Lavadora(220, 44);
         arrayObjetos[6] = new Television(500, 500);
-
-        arrayObjetos[7] = new Lavadora(10000,100000);
-
-        arrayObjetos[8] = new Television(-1,-15);
-
+        arrayObjetos[7] = new Lavadora(100,100);
+        arrayObjetos[8] = new Television(9999999,9999999);
         arrayObjetos[9] = new Lavadora(9999999, 999999);
 
-        System.out.println("La suma de los precios finales  es ");
 
+        int sumaLavadoras=0;
+        int sumaTelevisores=0;
         int suma=0;
         for (int i=0;i<arrayObjetos.length;i++){
-            suma+=arrayObjetos[i].precioFinal();
+
+            if (arrayObjetos[i] instanceof Lavadora){
+                sumaLavadoras+=arrayObjetos[i].precioFinal();
+
+            } else if(arrayObjetos[i] instanceof Television){
+                sumaTelevisores+=arrayObjetos[i].precioFinal();
+            }
+            suma = sumaLavadoras+sumaTelevisores;
         }
-        System.out.println("La suma de los precios finales  es "+suma);
+        System.out.println("La suma de los precios de LAVADORAS  es "+sumaLavadoras);
+        System.out.println("La suma de los precios TELEVISORES  es "+sumaTelevisores);
+        System.out.println("La suma de los precios TOTALES  es "+suma);
 
 
 
